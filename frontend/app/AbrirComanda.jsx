5faf38a9dbeff64cd8b366d46b2c01df17176708
Ruan Carlos Piccolli <ruanprogramacao200318@gmail.com>
2024-11-12 22:05:27 -0300

    criado o formulariopara escolher as datas de abertura e fechamento das comandas

--- frontend/app/AbrirComanda.jsx
+++ frontend/app/AbrirComanda.jsx
@@ -1,11 +1,37 @@
 import { Text,View } from "react-native";
+import { useState } from "react";
+import { NativeBaseProvider, Heading, Input } from "native-base";
 
-
-//colocar o formulario para o cadastro de itens
+//Criar o DatePicker
 export default function Abrir_Comanda(){
+    const [date, setDate] = useState(new Date())
     return(
-        <View style={{flex: 1, alignItems: 'center',justifyContent: 'center'}}>
-            <Text>Abertura de comandas</Text>
+        <View style={{flex: 1, alignItems: 'center',justifyContent: 'left', marginLeft: 5 }}>
+        <NativeBaseProvider>
+        <Heading>
+            Comandas
+          
+            </Heading>
+            <Heading fontSize={'sm'} marginTop={5}>
+                Data Abertura
+            </Heading>
+            <Input>
+            
+            </Input>
+            
+        <View>
+           
+           
+            <Heading fontSize={'sm'}>
+               Data fechamento
+            </Heading>
+            <Input>
+            </Input>
+
+
+        </View>
+
+        </NativeBaseProvider>
         </View>
 
 
